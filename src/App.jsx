7200d9030a908bdbd40c3a4c79e5b53d00a3a81d
Anmol Dethe <anmoldethe26@gmail.com>
2026-05-30 22:49:43 +0530

    Temp dashboard rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/DashboardTemp";
 import Tasks from "./pages/Tasks";
 import Notes from "./pages/Notes";
 import Timer from "./pages/Timer";
